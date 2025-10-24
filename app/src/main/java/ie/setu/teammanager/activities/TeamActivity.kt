cd4e0704df8174dbf1c4cde3fa90e54d9c489e4f
Calum Cullen <Calum.cullen66@gmail.com>
2025-10-24 15:53:01 +0100

    Added Comments TeamActivity

--- app/src/main/java/ie/setu/teammanager/activities/TeamActivity.kt
+++ app/src/main/java/ie/setu/teammanager/activities/TeamActivity.kt
@@ -10,11 +10,18 @@ import ie.setu.teammanager.databinding.ActivityTeamBinding
 import ie.setu.teammanager.main.MainApp
 import ie.setu.teammanager.models.TeamManagerModel
 
+//Deals with the adding and editing of teams in the app.
+
 class TeamActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTeamBinding
+    //Used to access the app and team list
     lateinit var app: MainApp
+
+    //create a new team object
     var team = TeamManagerModel()
+
+    //flagged fir checking if we are editing a team
     var editMode = false
     var editPosition = -1
 
@@ -23,11 +30,13 @@ class TeamActivity : AppCompatActivity() {
         binding = ActivityTeamBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //set up toolbar
         binding.toolbarAdd.title = title
         setSupportActionBar(binding.toolbarAdd)
 
         app = application as MainApp
 
+        //if its an exisitng team fill the details in the app
         if (intent.hasExtra("team_name")) {
             editMode = true
             binding.teamName.setText(intent.getStringExtra("team_name"))
@@ -39,6 +48,7 @@ class TeamActivity : AppCompatActivity() {
             editPosition = intent.getIntExtra("team_position", -1)
         }
 
+        //adds a team to the app
         binding.btnAdd.setOnClickListener {
             team.name = binding.teamName.text.toString()
             team.manager = binding.managerName.text.toString()
@@ -48,10 +58,13 @@ class TeamActivity : AppCompatActivity() {
 
             if (team.name.isNotEmpty()) {
                 if (editMode && editPosition >= 0) {
+                    //edit team
                     app.teams[editPosition] = team.copy()
                 } else {
+                    //add team
                     app.teams.add(team.copy())
                 }
+                //save team
                 app.saveTeams()
                 setResult(RESULT_OK)
                 finish()
@@ -60,7 +73,7 @@ class TeamActivity : AppCompatActivity() {
             }
         }
     }
-
+    //Cancel button in team creation view
         override fun onCreateOptionsMenu(menu: Menu): Boolean {
             menuInflater.inflate(R.menu.menu_team, menu)
             return super.onCreateOptionsMenu(menu)
